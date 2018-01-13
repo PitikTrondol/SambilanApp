7559c62732654497740117088df894c5328c89b0
PitikTrondol <emailafriandi@gmail.com>
2018-01-13 19:56:33 +0700

    [Afriandi] adding custom Application, refactor LandingPageActivity

---
+++ app/src/main/java/com/sambilan/sambilan/SambilanApplication.java
@@ -0,0 +1,12 @@
+package com.sambilan.sambilan;
+
+import android.app.Application;
+
+/**
+ * Created by Afriandi Haryanto on 1/13/2018.
+ */
+
+public class SambilanApplication extends Application {
+
+    
+}
