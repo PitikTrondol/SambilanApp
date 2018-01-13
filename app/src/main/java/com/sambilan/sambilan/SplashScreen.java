6c56ee2da8b92d00c8afdc70e1ee8e7339f1f1d6
Andhika Putranto <andhikaputra004@gmail.com>
2018-01-14 00:29:39 +0700

    Create Splash Screen

---
+++ app/src/main/java/com/sambilan/sambilan/SplashScreen.java
@@ -0,0 +1,30 @@
+package com.sambilan.sambilan;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+
+/**
+ * Created by Andhika Putranto on 1/13/2018.
+ */
+
+public class SplashScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.splash_screen);
+
+        final Handler handler =new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(),LandingPageActivity.class));
+                finish();
+            }
+        },3000);
+    }
+}
