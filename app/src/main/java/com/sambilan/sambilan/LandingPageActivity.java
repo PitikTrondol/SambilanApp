eaba0a18cf90234c341f9b6335a4609151cb0839
Solihin Febrian <solihinfebrian0@gmail.com>
2018-01-14 23:24:12 +0700

    [Solihin Febrian][feature/bottomNav]Create Bottom Navigation

--- app/src/main/java/com/sambilan/sambilan/LandingPageActivity.java
+++ app/src/main/java/com/sambilan/sambilan/LandingPageActivity.java
@@ -1,13 +1,43 @@
 package com.sambilan.sambilan;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class LandingPageActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    Toast.makeText(getApplicationContext(),"Beranda", Toast.LENGTH_SHORT).show();
+                    return true;
+                case R.id.navigation_add:
+                    Toast.makeText(getApplicationContext(),"Tambah",Toast.LENGTH_SHORT).show();
+                    return true;
+                case R.id.navigation_category:
+                    Toast.makeText(getApplicationContext(),"Kategori",Toast.LENGTH_SHORT).show();
+                    return true;
+                case R.id.navigation_me:
+                    Toast.makeText(getApplicationContext(),"Saya",Toast.LENGTH_SHORT).show();
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 }
