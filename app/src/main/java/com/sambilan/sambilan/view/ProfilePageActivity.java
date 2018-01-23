7664c3d24cc1a07b8ee8499260e0ffbb0fdd94ee
PitikTrondol <emailafriandi@gmail.com>
2018-01-24 06:26:46 +0700

    [Afriandi]add new screen

--- app/src/main/java/com/sambilan/sambilan/view/ProfilePageActivity.java
+++ app/src/main/java/com/sambilan/sambilan/view/ProfilePageActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.sambilan.sambilan.R;
