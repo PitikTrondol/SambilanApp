2716fcb79fb6ac90be16cdbced2672ccfc546cf8
Solihin Febrian <solihinfebrian0@gmail.com>
2018-02-01 00:13:57 +0700

    [Solihin Febrian]Memperbaiki detail job dan menunggu

---
+++ app/src/main/java/com/sambilan/sambilan/view/adapter/listener/ListMenungguListener.java
@@ -0,0 +1,10 @@
+package com.sambilan.sambilan.view.adapter.listener;
+
+/**
+ * Created by febrian on 31/01/18.
+ */
+
+public interface ListMenungguListener extends BaseRecyclerListener {
+
+    public void onClickBatalkan();
+}
