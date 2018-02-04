3c4256ef9c0df73a5f754f7805d375637f82a41a
PitikTrondol <emailafriandi@gmail.com>
2018-02-04 18:00:08 +0700

    [Afriandi] change fragment layout, delete some commited files

--- app/src/main/java/com/sambilan/sambilan/view/fragment/LandingPageFragment.java
+++ app/src/main/java/com/sambilan/sambilan/view/fragment/LandingPageFragment.java
@@ -54,7 +54,6 @@ public class LandingPageFragment extends Fragment {
     private SwipeRefreshLayout recyclerRefresher;
 
 
-
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -108,10 +107,12 @@ public class LandingPageFragment extends Fragment {
 
         return fragments;
     }
+
     private SwipeRefreshLayout.OnRefreshListener refreshListener = new SwipeRefreshLayout.OnRefreshListener() {
         @Override
         public void onRefresh() {
             listJobPresenter.getAllResources(jobCallback,3,5);
+
         }
     };
 
@@ -127,21 +128,10 @@ public class LandingPageFragment extends Fragment {
 
         @Override
         public void OnFailureResult(Throwable second) {
-            if (second instanceof HttpException) {
-                Toast.makeText(getActivity(),
-                        "JEMBUT " + ((HttpException) second).code(),
-                        Toast.LENGTH_SHORT).show();
-            } else if (second instanceof NullPointerException) {
-                Toast.makeText(getActivity(),
-                        "TELEK " + ((NullPointerException) second).getMessage(),
-                        Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(getActivity(),
-                        "TELEK " + second.getMessage(),
-                        Toast.LENGTH_SHORT).show();
 
-                System.out.println("TELEK "+second.getMessage());
-            }
+            Toast.makeText(getActivity(),
+                    "TELEK " + second.getMessage(),
+                    Toast.LENGTH_SHORT).show();
 
             progressBar.setVisibility(View.GONE);
             recyclerRefresher.setRefreshing(false);
