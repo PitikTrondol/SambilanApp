6c5a3b4660795597a86720b6f091517049ba347f
PitikTrondol <emailafriandi@gmail.com>
2018-02-04 16:18:15 +0700

    [Afriandi] Add google play service

---
+++ app/src/main/java/com/sambilan/sambilan/view/fragment/CategoryFragment.java
@@ -0,0 +1,10 @@
+package com.sambilan.sambilan.view.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Afriandi Haryanto on 2/3/2018.
+ */
+
+public class CategoryFragment extends Fragment {
+}
